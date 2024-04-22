0c5cd2ccc48c68100077f1dece3ec3ac84a754ba
ahmedfayez070 <ahmed070fayez@gmail.com>
2024-04-22 02:35:15 +0200

    imgs

--- src/pages/add/Add.jsx
+++ src/pages/add/Add.jsx
@@ -46,14 +46,14 @@ const Add = () => {
     setUploading(true);
     try {
       const cover = await upload(coverFile);
-      const imgs = Promise.all(
+      let images = Promise.all(
         [...files].map(async (file) => {
           const url = await upload(file);
           return url;
         })
       );
       setUploading(false);
-      dispatch({ type: "ADD_IMAGES", payload: { cover, imgs } });
+      dispatch({ type: "ADD_IMAGES", payload: { cover, imgs: images } });
     } catch (error) {
       console.log(error);
     }
